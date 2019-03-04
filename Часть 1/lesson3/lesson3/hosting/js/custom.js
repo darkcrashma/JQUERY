7ff53f6f94221f167a951cdcb06f5836ebe8c2c2
Albina <darkcrashma@mail.ru>
2019-03-04 17:26:18 +0800

    #Сделаны задания до пункта 2.3 не включительно

--- Часть 1/lesson3/lesson3/hosting/js/custom.js
+++ Часть 1/lesson3/lesson3/hosting/js/custom.js
@@ -1,6 +1,6 @@
 
-/**/
-
+/*---------------------------------------------------- Задание 1 -------------------------------------------------*/
+/*--------------------------------Разные выборки ------------------------------------------------*/
 // var allClasses= $('[class]');
 // var allID = $('[id]');
 // var allTags = $("*");
@@ -10,14 +10,13 @@
 // console.log(allTags);
 
 
-
-var allNames= $('[name]');
+// var allNames= $('[name]');
 // var allChecks = $(':checkbox');
 // var allInputs = $(":input");
 // var allBtn = $(":button");
 // var allRadio = $(":radio");
 //
-console.log(allNames);
+// console.log(allNames);
 // console.log(allChecks);
 // console.log(allInputs);
 // console.log(allBtn);
@@ -32,17 +31,108 @@ console.log(allNames);
 // console.log(allDisabled);
 
 
-
 // var listChildren=$('.nav > a');
 // console.log(listChildren);
 
-var multiAttr = $('[name="search_domain"],[class="pull-left"],[class="search-input"]').css({
-    'outline' : '2px solid red',
-    'background-color' : 'magenta'
-}); /*ПОЧЕМУ НЕ ИЩЕТ */
-console.log(multiAttr);
+// var multiAttr = $('[name="search_domain"],[class="pull-left search-input"]');
+// console.log(multiAttr);
 
 // var multiName = $('[name][class]');
 // console.log(multiName);
+
+/*---------------------------------------------------- Задание 1 -------------------------------------------------*/
+/*--------------------------------Клики на кнопки и ссылки------------------------------------------------*/
+
+// var allButtons = $('button');
+// console.log(allButtons);
+// var allLinks = $('a.btn');
+// console.log(allLinks);
+//
+// allButtons.on('click', function (e) {
+//     e.preventDefault();
+//     alert ('ДРАТУТИ! Вы жмякнули батОн!:)');
+// });
+// allLinks.on('click', function (e) {
+//     e.preventDefault();
+//     alert ('ДРАТУТИ! Вы жмякнули линк!:)');
+//     prompt ("Хотите задать какой-то вопрос?");
+//     confirm ("Перехотите :(");
+// });
+/*--------------------------------Скролл нав меню по якорям------------------------------------------------*/
+// $("#js-nav-domains").on('click', function(e) {
+//     e.preventDefault();
+//     var domainsOffset = $('#js-domain').offset().top;
+//      $('html,body').animate({
+//        scrollTop: domainsOffset
+//     }, 500);
+// });
+//
+// $("#js-nav-features").on('click', function(e) {
+//     e.preventDefault();
+//     var featuresOffset = $('#js-features').offset().top;
+//     $('html,body').animate({
+//         scrollTop: featuresOffset
+//     }, 500);
+// });
+// $("#js-nav-test").on('click', function(e) {
+//     e.preventDefault();
+//     var testOffset = $('#js-test').offset().top;
+//     $('html,body').animate({
+//         scrollTop: testOffset
+//     }, 500);
+// });
+// $("#js-nav-contacts").on('click', function(e) {
+//     e.preventDefault();
+//     var contactsOffset = $('#js-contacts').offset().top;
+//     $('html,body').animate({
+//         scrollTop: contactsOffset
+//     }, 500);
+// });
+
+/*--------------------------------Скролл вверх------------------------------------------------*/
+
+// $(document).scroll(function () {
+//     var $scrollTop = $(document).scrollTop();
+//     if($scrollTop > 845) {
+//         $('.toTheTop').addClass('toTheTop--active');
+//     } else {
+//         $('.toTheTop').removeClass('toTheTop--active');
+//     }
+// });
+//
+// $(".toTheTop").on('click', function(e) {
+//     e.preventDefault();
+//     $('html,body').animate({
+//         scrollTop: 0
+//     }, 500);
+// });
+
+/*--------------------------------Задание 2------------------------------------------------*/
+/*--------------------------------Фильтры------------------------------------------------*/
+//
+// var firstImg = $('img').first();
+// console.log(firstImg[0]);
+//
+// var lastImg = $('img').last();
+// console.log(lastImg[0]);
+//
+// var content = $('.container input');
+// console.log(content);
+
+// var inputMassive = $('input[type="text"]').closest($('.container'));
+// console.log(inputMassive);
+// $('.container').not(inputMassive);
+
+
+// Делаем табы с подчеркиванием на навигационном меню
+
+$('.nav__item').on('click', function (e) {
+    e.preventDefault();
+    if (!$(this).hasClass('nav__item--active')) {
+        $('.nav__item').removeClass('nav__item--active');
+    }
+    $(this).addClass('nav__item--active');
+});
+
 
 
